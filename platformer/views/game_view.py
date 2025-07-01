acc3b9105afc93abc36bb7ca675d6808e663f0fb
guyhadad <guy.hadad1903@gmail.com>
2025-07-01 18:00:40 +0300

    Add static camera in game setup

--- platformer/views/game_view.py
+++ platformer/views/game_view.py
@@ -1,5 +1,7 @@
 import arcade
+import math
 from platformer.views.view import View
+from platformer.constants import MAP_HEIGHT, ASPECT_RATIO
 from platformer.constants import TILE_SCALING, LAYER_NAME_GROUND
 
 class GameView(View):
@@ -8,6 +10,9 @@ class GameView(View):
 
         # Scene Object
         self.scene = None
+
+        # Camera object
+        self.camera = None
 
         
 
@@ -38,11 +43,28 @@ class GameView(View):
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
 
-        
+        # Set up the camera
+        self.camera = arcade.Camera2D(
+            arcade.types.Viewport(0, 0, math.floor(MAP_HEIGHT * ASPECT_RATIO) * self.tile_map.tile_width * TILE_SCALING, MAP_HEIGHT * self.tile_map.tile_height * TILE_SCALING),
+        )
+
+        # Set the viewport to match the window size
+        self.camera.viewport_width = self.window.width
+        self.camera.viewport_height = self.window.height
+
+
     def on_show_view(self):
         self.window.background_color = arcade.color.CORNFLOWER_BLUE
 
     def on_draw(self):
         if not self.started:
             self.setup()
+        
+        # Clear screen to the background color
+        self.clear()
+
+        # Activate camera
+        self.camera.use()
+
+        # Draw scene
         self.scene.draw()
